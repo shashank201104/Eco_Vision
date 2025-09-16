670f68917ed7cd26cccfc149f03576322ce1fbb3
Pratham Khare <prathamkhare222@gmail.com>
2025-09-16 23:06:46 +0530

    Add intial frontend code

---
+++ client_/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from "./pages/About.jsx"
+import CategoryDetail from "./pages/CategoryDetail.jsx"
+import Index from "./pages/Index.jsx"
+import NotFound from "./pages/NotFound.jsx"
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/category/:categoryId" element={<CategoryDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ client_/src/components/UserTestimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserTestimonials = () => {
+  return (
+    <div>UserTestimonials</div>
+  )
+}
+
+export default UserTestimonials
