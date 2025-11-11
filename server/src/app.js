e4e506392847846ab3cc0f8b66363e4ff00cb1ff
shashank201104 <shashanksaini201104@gmail.com>
2025-11-11 23:30:12 +0530

    auth routes added

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import ItemRoutes from "./routes/itemRoutes.js"
 import UploadRoutes from "./routes/uploadRoutes.js"
+import Auth from "./routes/Auth.js"
+import cookieparser from "cookie-parser"
 
 dotenv.config();
 connectDB();
@@ -14,8 +16,17 @@ const app = express();
 const PORT= process.env.PORT || 5000;
 
 //using cors for cross origin access and json for api's data parsing 
-app.use(cors());
+app.use(cors(
+    { origin: 'http://localhost:5173',
+    credentials: true }
+
+));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
+
+//route to check user authentication status(login/signup)
+app.use("/auth",Auth);
 
 //route to fetch recylable items's data from database.
 app.use("/items",ItemRoutes);
